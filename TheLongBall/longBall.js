9dc951c3bccb112dca01b2539432de4d521f7dc4
jhkessinger <jhkessinger@gmail.com>
2021-05-04 13:48:26 -0400

    Adding the air sound effect & fixing comments

--- TheLongBall/longBall.js
+++ TheLongBall/longBall.js
@@ -1,3 +1,4 @@
+// Loading canvas through index file
 var c = document.getElementById("canvas");
 var pullImage = c.getContext("2d");
 
@@ -7,31 +8,30 @@ var playerTop = new Image();
 var playerBottom = new Image();
 var stadium = new Image();
 
-// Need to find the actual png images
 baseball.src = "Images/baseball.png";
 playerTop.src = "Images/playerTop.png";
 playerBottom.src = "Images/playerBottom.png";
 stadium.src = "Images/stadium.png";
 
 // Loading audio clips
-
 var air = new Audio();
 var pointscored = new Audio();
 
 air.src = "Sounds/air.mp3";
 pointscored.src = "Sounds/pointscored.mp3";
 
-// these are variables for the ball, fielders, and movement
+// These are variables for the ball, fielders, and movement
 var ballX = 225;
 var ballY = 370; //starting points
-
-// gravity variable
 var gravity = 2;
-// defines the move variable for the ball to go up
+
+// Defines the move variable for the ball to go up
 document.addEventListener("keydown", moveUp); //any key works
-//adds an event handler to an element, calls the event and gives the function
+
+// Adds an event handler to an element, calls the event and gives the function
 function moveUp() {
   ballY -= 35;
+  air.play();
 }
 
 function draw() {
