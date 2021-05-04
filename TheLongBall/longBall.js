29b292245f72de9e877a95b89fbb095922c21cf8
jhkessinger <jhkessinger@gmail.com>
2021-05-04 11:15:20 -0400

    Loading audio clips

--- TheLongBall/longBall.js
+++ TheLongBall/longBall.js
@@ -1,7 +1,23 @@
+// Loading images
+var baseball = new Image();
+var playerTop = new Image();
+var playerBottom = new Image();
+
+// Need to find the actual png images
+//baseball.src =
+//playerTop.src =
+//playerBottom.src =
+
+// Loading audio clips
+
+var air = new Audio();
+var pointscored = new Audio();
+
+air.src = "Sounds/air.mp3";
+pointscored.src = "Sounds/pointscored.mp3";
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(288, 512);
 }
 
-function draw() {
-
-}
+function draw() {}
